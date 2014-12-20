277a5daa03a0081c474e6633cd3e95b04bab2c52
stefanius <KIT_kat_16>
2014-12-20 13:26:10 +0100

    Updated listviews

---
+++ src/Stef/SimpleCmsBundle/ListView/PageListView.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Stef\SimpleCmsBundle\ListView;
+
+class NewsListView extends AbstractListView
+{
+    protected function initProperties()
+    {
+        return [
+            'title',
+            'slug',
+            'created',
+            'modified'
+        ];
+    }
+
+    protected function initHeaders()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Titel',
+            'slug' => 'Slug',
+            'created' => 'Aangemaakt',
+            'modified' => 'Bewerkt'
+        ];
+    }
+
+}
