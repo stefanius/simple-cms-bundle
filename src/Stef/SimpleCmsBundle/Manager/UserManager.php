1a3d394beb25cef666335552a4c71e303b2b8d29
stefanius <KIT_kat_16>
2014-12-20 16:45:00 +0100

    Added User to CMS

---
+++ src/Stef/SimpleCmsBundle/Manager/UserManager.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Stef\SimpleCmsBundle\Manager;
+
+use Doctrine\Entity;
+use Stef\SimpleCmsBundle\Entity\News;
+use Stef\SlugManipulation\Manipulators\SlugManipulator;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class UserManager extends AbstractObjectManager {
+
+    /**
+     * @var SlugManipulator
+     */
+    protected $slugifier;
+
+    protected $repoName = 'StefSimpleCmsBundle:User';
+
+    /**
+     * @param ParameterBag $data
+     *
+     * @return Entity
+     */
+    public function create(ParameterBag $data)
+    {
+        /*
+        $news = new News();
+
+        $news->setTitle($data->get('title'));
+        $news->setBody($data->get('body'));
+        $news->setPicture($data->get('picture'));
+        $news->setSlug($this->slugifier->manipulate($news->getTitle() . '-' . rand(100 , 999)));
+
+        return $news; */
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function read($key)
+    {
+        $entity = parent::read($key);
+
+        if ($entity === null) {
+            $entity = $this->om->getRepository($this->repoName)->findOneBySlug($key);
+        }
+
+        return $entity;
+    }
+}
