cc7e6d410b0043a2076c024e900500b5cac71908
Stefanius <sgrootveld@gmail.com>
2015-11-27 21:14:08 +0100

    Update extension.

--- src/Stef/SimpleCmsBundle/Twig/Extension/AssetExistsExtension.php
+++ src/Stef/SimpleCmsBundle/Twig/Extension/AssetExistsExtension.php
@@ -6,45 +6,61 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 class AssetExistsExtension extends \Twig_Extension
 {
-
+    /**
+     * @var KernelInterface
+     */
     private $kernel;
 
+    /**
+     * @param KernelInterface $kernel
+     */
     public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
     }
 
+    /**
+     * @return array
+     */
     public function getFunctions()
     {
         return array(
-            'asset_exists' => new \Twig_SimpleFunction($this, 'asset_exists'),
+            new \Twig_SimpleFunction('asset_exists', array($this, 'assetExists')),
         );
     }
 
-    public function asset_exists($path)
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
+    public function assetExists($path)
     {
         if (substr($path,0, 7) === 'http://') {
             return $this->checkWebAdress($path);
         }
+
         $webRoot = realpath($this->kernel->getRootDir() . '/../web/');
         $toCheck = realpath($webRoot . $path);
 
         // check if the file exists
-        if (!is_file($toCheck))
-        {
-
+        if (!is_file($toCheck)) {
             return false;
         }
 
         // check if file is well contained in web/ directory (prevents ../ in paths)
-        if (strncmp($webRoot, $toCheck, strlen($webRoot)) !== 0)
-        {
+        if (strncmp($webRoot, $toCheck, strlen($webRoot)) !== 0) {
             return false;
         }
 
         return true;
     }
 
+    /**
+     * @param $path
+     *
+     * @return bool
+     */
     protected function checkWebAdress($path)
     {
         $headers = get_headers($path);
@@ -52,6 +68,9 @@ class AssetExistsExtension extends \Twig_Extension
         return $headers[0] === "HTTP/1.1 200 OK";
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'asset_exists';
